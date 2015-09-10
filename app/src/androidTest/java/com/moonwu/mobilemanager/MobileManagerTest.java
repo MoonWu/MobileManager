1c320e3c54139579578f2def7f12ef1431187495
MoonWu <xinwu1987@126.com>
2015-09-10 17:49:51 +0800

    连接网络 json解析

--- app/src/androidTest/java/com/moonwu/mobilemanager/MobileManagerTest.java
+++ app/src/androidTest/java/com/moonwu/mobilemanager/MobileManagerTest.java
@@ -1,13 +1,14 @@
 package com.moonwu.mobilemanager;
 
 import android.app.Application;
+import android.test.AndroidTestCase;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class MobileManagerTest extends AndroidTestCase{
+    public void testStreamTool(){
+            
     }
 }
--- app/src/main/java/com/moonwu/mobilemanager/SplashActivity.java
+++ app/src/main/java/com/moonwu/mobilemanager/SplashActivity.java
@@ -4,10 +4,25 @@ import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.moonwu.mobilemanager.utils.StreamTool;
+
+import org.apache.http.HttpConnection;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class SplashActivity extends Activity {
 
+    private static final String TAG = "SplashActivity";
     private TextView tv_splash_version = null;
 
     @Override
@@ -16,6 +31,54 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.activity_splash);
         tv_splash_version = (TextView)findViewById(R.id.tv_splash_version);
         tv_splash_version.setText("版本号:" + getVersionName());
+
+        //检查更新
+        checkUpdate();
+    }
+
+
+    /**
+     * 检查是否存在更新,如果有就更新
+     */
+    private void checkUpdate() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(getString(R.string.serverurl));
+                    //连接网络
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(5000);
+                    int code = conn.getResponseCode();
+                    if (200 == code) {
+
+                        //联网成功
+                        InputStream is = conn.getInputStream();
+                        String result = StreamTool.readInputStream(is);
+                        Log.i(TAG, "联网成功" + result);
+
+                        //json解析
+                        JSONObject job = new JSONObject(result);
+                        String version = (String) job.get("version");
+                        String description = (String) job.get("description");
+                        String apkurl = (String) job.get("apkurl");
+                        Log.i(TAG, "解析成功: " + version + "|" + description +
+                                "|" + apkurl);
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     /**
